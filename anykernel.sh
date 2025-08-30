082cd10b0efc658b3d219bdb76297ea465496569
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-08-31 01:30:50 +0700

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Teletubies Kernel by Malkist 
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.systemless=0
@@ -39,6 +39,10 @@ patch_vbmeta_flag=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
 ## AnyKernel boot install
 dump_boot;
