a17c86b0063849a1af802d6e3cd74746f9bc6fda
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-04-05 18:21:54 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -7,65 +7,27 @@ properties() { '
 kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=mido
-device.name2=santoni
-device.name3=j6primelte
-device.name4=j4primelte
-device.name5=a7xelte
-device.name6=a7xeltexx
-device.name7=a7xelteub
-device.name8=a7xeltedo
-device.name9=CPH2083
-device.name10=OP4BFB
-supported.versions=
+device.name1=ginkgo
+device.name2=willow
+supported.versions=10.0-16.0
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
-
-## AnyKernel boot install
+## AnyKernel install
 dump_boot;
-
 write_boot;
-## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
+## end install
 
