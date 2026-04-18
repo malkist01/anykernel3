37b10e1ea915e5c553f7dcb7282c748796a461f6
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-04-18 15:55:01 +0700

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -1,33 +1,106 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
-kernel.string=
+kernel.string=AnjaniLaurens Kernel by Malkist
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=ginkgo
-device.name2=willow
-supported.versions=10.0-16.0
+device.name2=
+supported.versions=
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
 
+# boot shell variables
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-## AnyKernel install
-dump_boot;
-write_boot;
-## end install
+# boot install
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
+# Check for IR HAL type
+if [ -f /vendor/bin/hw/android.hardware.ir-service.lineage ]; then
+	ui_print "LIRC-based IR HAL detected"
+else
+	ui_print "Legacy spidev IR HAL detected"
+	patch_cmdline "legacy_ir_hal" "legacy_ir_hal=1"
+fi
+
+# Get Android version from build.prop
+android_ver=$(file_getprop /system/build.prop ro.build.version.release)
+
+# Convert to integer (strip potential decimal points)
+android_ver=${android_ver%%.*}
+
+# Check if Android version is 11 or lower
+if [ "$android_ver" -le 11 ] 2>/dev/null; then
+    patch_cmdline "legacy_timestamp_source" "legacy_timestamp_source=1"
+    ui_print "Legacy timestamp workaround enabled"
+else
+    patch_cmdline "legacy_timestamp_source" "legacy_timestamp_source=0"
+    ui_print "Timestamp patch not needed"
+fi
+
+if mountpoint -q /data; then
+  # Optimize F2FS extension list (@arter97)
+  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
+    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
+
+    # Skip update if our list is already active
+    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
+      echo "Extension list up-to-date: $list_path"
+      continue
+    fi
+
+    ui_print "  • Optimizing F2FS extension list"
+    echo "Updating extension list: $list_path"
+
+    echo "Clearing extension list"
+
+    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
+    list_len="$(cat $list_path | wc -l)"
+    cold_count="$((list_len - hot_count))"
+
+    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
+    hot_list="$(tail -n$cold_count $list_path)"
+
+    for ext in $cold_list; do
+      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
+    done
+
+    for ext in $hot_list; do
+      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
+    done
+
+    echo "Writing new extension list"
+
+    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
+      [ ! -z $ext ] && echo "[c]$ext" > $list_path
+    done
+
+    for ext in $(cat $home/f2fs-hot.list); do
+      [ ! -z $ext ] && echo "[h]$ext" > $list_path
+    done
+  done
+fi
+
+flash_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_dtbo;
