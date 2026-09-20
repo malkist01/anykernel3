cce467c9e02065b8bdcd6d2284afd3063484339d
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-09-20 16:47:15 +0700

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -1,42 +1,34 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+
 ## AnyKernel setup
 # begin properties
 properties() { '
-do.devicecheck=1
+kernel.string=KAGA-KOKO
+do.devicecheck=0
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=vince
-device.name2=ysl
-device.name3=rosy
-device.name4=tiffany
-device.name5=daisy
-device.name6=sakura
-device.name7=daikura
-device.name8=mido
-device.name9=Mi8953v1
-device.name10=Mi8953v2
+device.name1=OP4BFB
+device.name2=CPH2083
+device.name2=Nightmare
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/platform/bootdevice/by-name/boot;
 is_slot_device=0;
-ramdisk_compression=auto;
+
+## Karena tidak ada ramdisk
+split_boot=false;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 750 750 $ramdisk/*;
-
 
 ## AnyKernel install
 dump_boot;
